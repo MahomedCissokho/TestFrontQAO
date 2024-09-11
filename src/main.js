8bbdecfd74cadbac8cbb0709bc3d3126409913f7
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-09-11 15:43:32 +0000

    some changes for security

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import './style.css'
 import router from './router'
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtEyhcrjv9OxbMUCpscu2nBDeroZ3IdLk",
-  authDomain: "galery-bea7f.firebaseapp.com",
-  projectId: "galery-bea7f",
-  storageBucket: "galery-bea7f.appspot.com",
-  messagingSenderId: "650748747345",
-  appId: "1:650748747345:web:b0cbf2b286934f62c4a270",
-  measurementId: "G-LPSXBF2XFB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 app.use(createPinia())
