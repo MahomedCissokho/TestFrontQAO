0d3e1344e57a0159933497d039796bad4397cfd1
Mahomed Cissokho <mahomedcissokho@esp.sn>
2024-09-12 15:22:54 +0000

    some changes

--- src/main.js
+++ src/main.js
@@ -4,17 +4,7 @@ import App from './App.vue'
 import './style.css'
 import router from './router'
 import { initializeApp } from "firebase/app";
-
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+import { firebaseConfig } from './firebaseConfig'
 
 initializeApp(firebaseConfig);
 
@@ -22,6 +12,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 
 app.mount('#app')
